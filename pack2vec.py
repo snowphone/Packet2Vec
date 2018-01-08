13dfc3fe45d22eb6643743eba6c91d5a1af589bf
Moon Junoh <mjo970625@gmail.com>
2018-01-08 16:45:44 +0900

    입출력을 파일로 주고받도록 변경

--- pack2vec.py
+++ pack2vec.py
@@ -5,6 +5,7 @@ import sys
 def main():
     '''
     인자: 직렬화된 패킷 덤프들
+    출력: '덤프 이름'.pattern 형태
     '''
     if len(sys.argv) <= 1:
         #sample data
@@ -17,17 +18,20 @@ def main():
         patterns.append(re.compile(rule))
 
     for name in sys.argv[1:]:
+        output = open(name+".pattern")
         packets = Parser.Deserialize(name)
         for packet in packets:
+            print("checking for", packets[0], "last packet:", packets[-1][0])
             payload = packet[-1]
             for pattern in patterns:
                 malware_payload = pattern.search(payload)
                 if not malware_payload:
                     continue
-                print("Rule:", regex_rule, 
-                "\nDetected pattern:", malware_payload.group(),
-                "\nPayload:", payload)
+                log = "Rule: "+ regex_rule+ "\nDetected pattern: "+ malware_payload.group()+ "\nPayload: "+ payload + "\n\n"
+                output.write(log)
+
                 break   #examine next payload
+        output.close()
     return
 
 if __name__ == "__main__":
