0e2fd79fbaff86ffcf4a49c0cb5657815d7aa861
Moon Junoh <mjo970625@gmail.com>
2018-01-10 22:44:44 +0900

    한 패킷에 대해 multiprocess.Pool 적용(병렬화)

--- Snort.py
+++ Snort.py
@@ -1,15 +1,28 @@
+import multiprocessing
+
+
 def Serialize(path, value):
     with open(path,mode="wb+") as output:
         pickle.dump(value, output)
     return
 
+def __examine__(record):
+    packet, patterns = record
+    payload = packet[-1]
+    print("checking for", packet[0])
+    for regexEngn in patterns:
+        cand = regexEngn.search(payload)
+        if cand:
+            return True, (packet[0], regexEngn.pattern, cand.group(), payload)
+    return False, None
+
 def Search(packets, patterns):
     '''
     snort_rule에 걸리는 패킷을 모아 (패킷 시각, 정규식, 일치한 패턴, 페이로드)를 리스트에 모아 반환한다.
     '''
-    log = [(packet[0], regexEngn.pattern, regexEngn.search(packet[-1]).group(), packet[-1])
-    for packet in packets
-    for regexEngn in patterns
-    if print("checking for", packet[0], ", last packet:", packets[-1][0]) or regexEngn.search(packet[-1])]
+
+    records = ((packet,patterns) for packet in packets) #packet: 시각, 송신자, 수신자, 페이로드
+    p = multiprocessing.Pool()  #auto detect threads
+    log = [tpl for pred, tpl in p.map(__examine__, records) if pred]
 
     return log
