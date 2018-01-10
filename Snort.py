68c682a59ddcf77b548fee8831c2b6480b7485c2
Moon Junoh <mjo970625@gmail.com>
2018-01-10 23:05:00 +0900

    pool.starmap을 사용하여 함수에 다인자 전달이 가능해짐.

--- Snort.py
+++ Snort.py
@@ -7,10 +7,9 @@ def Serialize(path, value):
         pickle.dump(value, output)
     return
 
-def __examine__(record):
-    packet, patterns = record
+def __examine__(packet, patterns, lastTime):
     payload = packet[-1]
-    print("checking for", packet[0])
+    print("checking for", packet[0], "last packet:", lastTime)
     for regexEngn in patterns:
         cand = regexEngn.search(payload)
         if cand:
@@ -22,8 +21,9 @@ def Search(packets, patterns):
     snort_rule에 걸리는 패킷을 모아 (패킷 시각, 정규식, 일치한 패턴, 페이로드)를 리스트에 모아 반환한다.
     '''
 
-    records = ((packet,patterns) for packet in packets) #packet: 시각, 송신자, 수신자, 페이로드
-    p = multiprocessing.Pool()  #auto detect threads
-    log = [tpl for pred, tpl in p.map(__examine__, records) if pred]
+    pool = multiprocessing.Pool()  #auto detect threads
+
+    records = ((packet, patterns, packets[-1][0]) for packet in packets) 
+    log = [tpl for pred, tpl in pool.starmap(__examine__, records) if pred] #pred 를 통해서 filter의 효과를 구현
 
     return log
