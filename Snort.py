c655e65e82c74c995493f438bbec407d7cd424f0
Moon Junoh <mjo970625@gmail.com>
2018-01-10 19:54:26 +0900

    불필요한 rule 인자를 regexEngn.pattern으로 대체

--- Snort.py
+++ Snort.py
@@ -3,18 +3,18 @@ def __serialize__(path):
         pickle.dump(log, output)
     return
 
-def Search(path, packets, rules, patterns):
+def Search(path, packets, patterns):
     '''
     snort_rule에 걸리는 패킷을 모아 (패킷 시각, 정규식, 일치한 패턴, 페이로드)를 직렬화해 저장한다.
     '''
     for packet in packets:
         print("checking for", packet[0], ", last packet:", packets[-1][0])
         payload = packet[-1]
-        for regexEngn, regexRule in zip(patterns, rules):
+        for regexEngn in patterns:
             malware_payload = regexEngn.search(payload)
             if not malware_payload:
                 continue
-            log.append((packet[0], regexRule, malware_payload.group(0), payload))
+            log.append((packet[0], regexEngn.pattern, malware_payload.group(0), payload))
             print("---Pattern matched---")
             break
     __serialize__(path)
