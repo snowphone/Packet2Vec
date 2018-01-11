c3ba515382eac07c2a8dcb678c8fd682a79b58b9
Moon Junoh <mjo970625@gmail.com>
2018-01-11 15:47:17 +0900

    word2vec 적용

--- packet2vec.py
+++ packet2vec.py
@@ -4,25 +4,44 @@ import Inspect
 import sys
 import gensim.models.word2vec as w2v
 
+def ExtractPayload(packets):
+    return [packet[-1] for packet in packets]
+
+def SplitPayload(payload, length=20, pattern=None):
+    '''
+    payload를 length 단위로 잘라 string list로 반환한다.
+    regexEngn이 인자로 들어온 경우 일치한 패턴을 중심으로 length 단위로 자른다.
+    '''
+    if pattern is None:
+        return [payload[i:i+length] for i in range(0,len(payload),length)]
+
+    idx = payload.find(pattern)
+    prePattern = payload[:idx]
+    postPattern = payload[idx + len(pattern) :]
+    return [word[::-1]  for word in SplitPayload(prePattern[::-1],length)] + [pattern] + SplitPayload(postPattern,length)
+
 def main():
-    week = ["mon", "tue", "wed", "thu"]
-    paths = ["trainData/" + a + "side.tcpdump_" + day + ".ascii_out.ser_malware" for day in week for a in ["in", "out"]]
+    '''
+    학습에 초점
+    '''
+    sys.argv = [None, "outside.tcpdump_fri.ascii_out.ser_malware", "inside.tcpdump_fri.ascii_out.ser_malware"]
 
-    trainData = []
-
-    for path in paths:
-        f = open(path,encoding="utf8")
-        trainData.append(f.read())
-        f.close()
-
-    model = w2v.Word2Vec(sys.argv[1])
-
+    #get malware packets
     for path in sys.argv[2:]:
-        packets = Parser.Deserialize(path)
-        sentences = [packet[-1] for packet in packets]
+        malware_packets = Parser.Deserialize(path)
+        malware_payloads = ExtractPayload(malware_packets)
+        matched_string = [[packet[2]] for packet in malware_packets]
 
-        for time,rule, matched, payload in packets:
-            print(model.most_similar(positive=[matched]))
+    length = 40
+    trainData = [SplitPayload(payload, length,regexEngn) for time,rule,regexEngn,payload in malware_packets]
+
+    model = w2v.Word2Vec(sentences=trainData,min_count=0)
+
+    packets = Parser.Deserialize(sys.argv[1])
+    sentences = [packet[-1] for packet in packets]
+
+    for time, rule, matched, payload in packets:
+        print(model.most_similar(positive=[matched]))
 
     return
 
