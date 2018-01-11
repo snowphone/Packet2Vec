fdc930b1a5b2f9d7b2f82ebe6033ac61e22d8e9d
root <root@nct.(none)>
2018-01-11 11:24:58 +0900

    Inspect.py를 단독으로도 사용 가능하게 변경

--- Inspect.py
+++ Inspect.py
@@ -18,5 +18,22 @@ def Inspect(rulePath, *args):
 
     return
 
+def main():
+    '''
+    인자: regex_pattern, packets1, packets2, ...
+    출력: '덤프 이름'.mal 형태
+    '''
+    if len(sys.argv) < 2:
+        return
+
+    with open(sys.argv[1]) as f:
+        rules = [rule.rstrip() for rule in f.readlines()]
+    patterns = [re.compile(rule) for rule in rules]
+
+    [Snort.Serialize(path + "_malware", Snort.Search(Parser.Deserialize(path), patterns)) 
+    for path in sys.argv[2:] ]
+
+ 
+
 if __name__ == "__main__":
-    Inspect()
+    main()
