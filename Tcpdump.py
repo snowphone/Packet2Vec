b6293413e074bcb8611510d0f53bf887049814ba
Moon Junoh <mjo970625@gmail.com>
2018-01-12 12:33:08 +0900

    Parse 함수 수정

--- Tcpdump.py
+++ Tcpdump.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import os
+import multiprocessing
 import pickle
 '''
 tcpdump 파일을 파이썬에서 다룰수 있게 하는 것이 목표.
@@ -10,11 +11,11 @@ tcpdump 파일을 파이썬에서 다룰수 있게 하는 것이 목표.
 변경점: packet의 길이 요소를 제거
 '''
 
-    '''
-    [packet for packet in packets] 의 형태로 파싱
-    각 패킷은  [시간, 송신 IP.port, 수신 IP.port, payload] 의 형태로 저장되어 있다.
-    Get() 메소드를 통하여 리스트를 가져올 수 있다.
-    '''
+'''
+[packet for packet in packets] 의 형태로 파싱
+각 패킷은  [시간, 송신 IP.port, 수신 IP.port, payload] 의 형태로 저장되어 있다.
+Get() 메소드를 통하여 리스트를 가져올 수 있다.
+'''
 def Unpack(path):
     '''
     tcpdump를 이용해 .unpacked 확장자로 언팩한다.
@@ -26,19 +27,18 @@ def Unpack(path):
 
 def Parse(filePath):
     '''
-    각 패킷을 원소로 가지는 list 반환
+    각 패킷을 원소로 가지는 리스트를 반환한다.
     각 패킷은  [시간, 송신 IP.port, 수신 IP.port, payload] 의 형태로 저장되어 있다.
     '''
     with open(filePath) as f:
-        lines = f.readline()
+        lines = f.readlines()
 
-    header = r"^(\d{2}:\d{2}:\d{2}\.\d{6}) IP (.+) > (.+?)(?=:\s)"
+    header = r"^(\d{2}:\d{2}:\d{2}\.\d{6}) IP (.+) > (.+?)(?=:\b)"
     # 00:00:00.000000 IP 1.1.1.1.portNum > 2.2.2.2.portNum
     # 가장 마지막 캡쳐를 살펴보자
     # .+?에서 ?는 +가 non-greedy 하게 소비하도록 한다. 따라서 긍정형 전방 탐색을 하는 과정에서 가장 처음으로 발견한 : 앞에서 수신 아이피.포트가 추출된다.
     hdrPattern = re.compile(header)
-    testpacket = re.compile(r"^\d{2}:\d{2}:\d{2}[.]\d{6}\b")
-    hexIndex = re.compile(r"\b0x[0-9a-fA-F]{4}:\b")
+    timePattern = re.compile(r"^\d{2}:\d{2}:\d{2}\.\d{6}") #시간정보만 확인
     ret = []
     bTestPacket = False
     cnt = 0
@@ -47,9 +47,9 @@ def Parse(filePath):
 
     for line in lines:
         cnt += 1
-        if cnt/length  >= threadhold:
+        if cnt/length  >= threshold:
             print("진행률:", cnt/length * 100, "%")
-            threadhold += 0.1
+            threshold += 0.1
 
         headerInfo = hdrPattern.search(line)
         if headerInfo:
@@ -59,19 +59,19 @@ def Parse(filePath):
             packet.append("")
 
             ret.append(packet)
-        elif bTestPacket or testpacket.search(line):       #test packet 같은 무의미한 패킷 패스
+        elif bTestPacket or timePattern.search(line):       #test packet 같은 무의미한 패킷 패스
             bTestPacket = True
             continue
         else:
-            ret[-1][-1] += hexIndex.sub("", line)     
+            ret[-1][-1] += line
                 
     return ret
 
 
 def Deserialize(filename):
-'''
-pickle을 통해 역 직렬화 함
-'''
+    '''
+    pickle을 통해 역 직렬화 함
+    '''
     with open(filename, "rb") as f:
         ret = pickle.load(f)
     return ret
@@ -82,28 +82,14 @@ def Serialize(path, data):
     return
 
 def main():
-'''
-인자: dump 파일, output 파일 이름, 파싱 방법(ascii, hex)
-Pickle을 통해 binary로 직렬화
-'''
-if len(sys.argv) > 2:
-    filename = sys.argv[1]
-    output = sys.argv[2]
-    parser = Parser(filename)
-    packets = parser.Parse()
-    with open(output, "wb") as f:
-        pickle.dump(packets, f)
-else:
-    filename = "hexoutput"
-    parser = Parser(filename)
-    packets = parser.Parse()
-    for packet in packets:
-        print("시간:", packet[0])
-        print("송신자:", packet[1])
-        print("수신자:", packet[2])
-        print("-------------Payload----------")
-        print(packet[-1])
-        print()
+    p = multiprocessing.Pool()
+    #p.map(Unpack, sys.argv[1:])
+    parsed = p.map(Parse, sys.argv[1:])
+    names = [name+".ser" for name in sys.argv[1:]]
+    p.starmap(Serialize, zip(names, parsed))
+
+    print("Done!")
+    return
 
 if __name__ == "__main__":
-main()
+    main()
